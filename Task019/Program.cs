eaf4dc5655509751051eeab62ec432d5ef4c05a4
aleksbul <120058706+aleksbul@users.noreply.github.com>
2022-12-22 13:19:15 +0200

    Update Program.cs

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -27,28 +27,3 @@ if (number > 0)
 }
 else Console.WriteLine("Введите пожалуйста положительное пятизначное число");
 
-
-
-
-
-
-// // Напишите программу, которая выводит третью цифру заданного числа 
-// или сообщает, что третьей цифры нет.
-
-// Console.WriteLine("Введите число");
-// int number = Convert.ToInt32(Console.ReadLine());
-// number = Math.Abs(number);
-
-// string str = number.ToString();
-// if (str.Length >= 3)
-// {
-//     int[] array = new int[str.Length];
-//     for (int i = 0; i < str.Length; i++)
-//     {
-//         array[i] = int.Parse(str[i].ToString());
-//     }
-//     Console.WriteLine(array[2]);
-// }
-// else
-//     Console.WriteLine("В заданном числе третьей цифры нет");
-
